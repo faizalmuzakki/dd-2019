14bfcbdd6b44e451b1f52b2d12cf63ec967e3e34
SherryVine <faizal.muzakki@gmail.com>
2019-03-06 10:00:12 +0700

    init

---
+++ provision/bootstrap-first.sh
@@ -0,0 +1,18 @@
+# Changing the APT sources.list to kambing.ui.ac.id
+sudo cp '/vagrant/config/sources.list' '/etc/apt/sources.list'
+
+# Updating the repo with the new sources
+sudo apt-get update -y
+
+# Setting input for MySQL root password
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password admin'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password admin'
+
+# Install MySQL Server
+sudo apt-get install mysql-server mysql-client -y
+
+# Copy MySQL configuration
+# sudo cp '/vagrant/config/mysqld-first.cnf' '/etc/mysql/mysql.conf.d/mysqld.cnf'
+
+# Restart MySQL service
+sudo service mysql restart
